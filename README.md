09cd81e274ee93168670014552706fd9922db835
david-fernando <davidfernandodamata21@gmail.com>
2023-06-19 11:45:44 -0300

    Add all

---
+++ README.md
@@ -0,0 +1,2 @@
+# binarytraders
+Site binarytraders.net
---
+++ js/index.js
@@ -0,0 +1,40 @@
+function changeUrlAccordingToTheLanguage(){
+  const language = window.navigator.language;
+
+  const returnUrlAccordingToTheLanguage = {
+      'pt':'https://pt.binarytraders.net',
+      'pt-BR':'https://pt.binarytraders.net',
+      'de':'https://de.binarytraders.net/',
+      'it':'https://it.binarytraders.net/',
+      'es':'https://es.binarytraders.net/',
+      'en':'https://en.binarytraders.net/',
+      'fr':'https://fr.binarytraders.net/',
+  }
+
+  const url = returnUrlAccordingToTheLanguage[language] || 'https://en.binarytraders.net/'
+
+  //O comentário abaixo só está ali para evitar o redirecionamento. Remova esse e o comentário abaixo depois de pronto, ou quando for necessário.
+  // window.location.href = url;
+}
+
+function showMoreText(){
+  const button = document.querySelector('.showMore')
+  const moreText = document.getElementById('more-text')
+  const ellipsis = document.getElementById('ellipsis')
+  const showMoreTextButton = document.querySelector('.showMoreTextButton')
+
+  button.addEventListener('click', () => {
+    const isOpenShowMore = (moreText.style.display === 'block')? true : false    
+      if(!isOpenShowMore){
+        moreText.style.display = 'block'
+        ellipsis.style.display = 'none'
+        return showMoreTextButton.innerHTML = 'Show less'
+      }
+
+      moreText.style.display = 'none'
+      ellipsis.style.display = 'inherit'
+      return showMoreTextButton.innerHTML = 'Show more'
+  })
+}
+showMoreText()
+changeUrlAccordingToTheLanguage()
