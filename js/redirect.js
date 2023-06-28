1b78c4cb6fbffb94699da9acdbd387d49fdc3126
david-fernando <davidfernandodamata21@gmail.com>
2023-06-28 00:38:58 -0300

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -2,19 +2,15 @@ function changeUrlAccordingToTheLanguage(){
   const language = window.navigator.language;
 
   const returnUrlAccordingToTheLanguage = {
-      'pt':'https://pt.binarytraders.net',
-      'pt-BR':'https://pt.binarytraders.net',
-      'de':'https://de.binarytraders.net/',
-      'it':'https://it.binarytraders.net/',
-      'es':'https://es.binarytraders.net/',
-      'en':'https://en.binarytraders.net/',
-      'fr':'https://fr.binarytraders.net/',
+      'pt':'https://binarytraders.vercel.app/pt',
+      'pt-BR':'https://binarytraders.vercel.app/pt',
+      'es':'https://binarytraders.vercel.app/es',
+      'en':'https://binarytraders.vercel.app/en',
   }
 
-  const url = returnUrlAccordingToTheLanguage[language] || 'https://en.binarytraders.net/'
-
-  //O comentário abaixo só está ali para evitar o redirecionamento. Remova esse e o comentário abaixo depois de pronto, ou quando for necessário.
-  // window.location.href = url;
+  const url = returnUrlAccordingToTheLanguage[language] || 'https://binarytraders.vercel.app/en'
+  
+  window.location.href = url;
 }
 
 changeUrlAccordingToTheLanguage()
