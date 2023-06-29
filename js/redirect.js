2f8aec06f39f2b1fe9fe57cd325887022eaeafe2
david-fernando <davidfernandodamata21@gmail.com>
2023-06-29 00:21:59 -0300

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,14 +1,17 @@
 function changeUrlAccordingToTheLanguage(){
   const language = window.navigator.language;
 
+  if (language === 'en', !languages in returnUrlAccordingToTheLanguage) {
+    return;
+  }
+
   const returnUrlAccordingToTheLanguage = {
       'pt':'https://binarytraders.vercel.app/pt',
       'pt-BR':'https://binarytraders.vercel.app/pt',
       'es':'https://binarytraders.vercel.app/es',
-      'en':'https://binarytraders.vercel.app/en',
   }
 
-  const url = returnUrlAccordingToTheLanguage[language] || 'https://binarytraders.vercel.app/en'
+  const url = returnUrlAccordingToTheLanguage[language] || 'https://binarytraders.vercel.app'
   
   window.location.href = url;
 }
