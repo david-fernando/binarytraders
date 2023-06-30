c42a6ff69c5c97a50430aca5a4b6c3239d6bbfda
david-fernando <davidfernandodamata21@gmail.com>
2023-06-29 21:44:42 -0300

    Update URLS

--- js/redirect.js
+++ js/redirect.js
@@ -3,16 +3,16 @@ function changeUrlAccordingToTheLanguage(){
   const chosenLanguage = localStorage.getItem('chosen-language') || ''
 
   const returnUrlAccordingToTheLanguage = {
-      'pt':'https://binarytraders.vercel.app/pt',
-      'pt-BR':'https://binarytraders.vercel.app/pt',
-      'es':'https://binarytraders.vercel.app/es',
+      'pt':'https://pt.binarytraders.net',
+      'pt-BR':'https://pt.binarytraders.net',
+      'es':'https://es.binarytraders.net',
   }
 
   if (language === 'en' || !(language in returnUrlAccordingToTheLanguage) || chosenLanguage === 'english') {
     return;
   }
 
-  const url = returnUrlAccordingToTheLanguage[language] || 'https://binarytraders.vercel.app'
+  const url = returnUrlAccordingToTheLanguage[language] || 'https://binarytraders.net'
   
   window.location.href = url;
 }
