a3167b9389a56273bbb8d463c621eeb69a85d49d
david-fernando <davidfernandodamata21@gmail.com>
2023-07-12 18:11:37 -0300

    Add importHtmlModules.js

---
+++ tools/js/importHtmlModules.js
@@ -0,0 +1,10 @@
+function loadHTML(file, elementId) {
+  fetch(file)
+    .then(response => response.text())
+    .then(html => {
+      const element = document.getElementById(elementId);
+      element.innerHTML = html;
+    });
+}
+
+loadHTML('trendindicator.html', 'trend-indicator');
