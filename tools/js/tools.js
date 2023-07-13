e1093d1c6d748436ea0d0eca9a7a60f98ca30210
david-fernando <davidfernandodamata21@gmail.com>
2023-07-13 18:52:21 -0300

    Add fetchCatalog

--- tools/js/tools.js
+++ tools/js/tools.js
@@ -1,9 +1,17 @@
 const select = document.getElementById("my-select");
+const timezoneSelect = document.getElementById("timezone-select");
+const directionSelect = document.getElementById("direction-select");
+const timeframeSelect = document.getElementById("timeframe-select");
+const daylistSelect = document.getElementById("daylist-select");
+const accuracyInput = document.getElementById("accuracy");
+const catalogDays = document.getElementById("catalogDays");
+const martingaleSelect = document.getElementById("martingale-select");
+const catalogButton = document.querySelector('.catalog-button')
+const selectedPairs = [];
 
-select.addEventListener("change", function () {
-    const selectedItems = [];
-    const selectedOptions = select.selectedOptions;
+select.addEventListener("change", () => {
     let previousValue = []
+    const selectedOptions = select.selectedOptions;
     let closeButton = null
     for (let index = 0; index < selectedOptions.length; index++) {
         const value = selectedOptions[index].value;
@@ -23,7 +31,7 @@ select.addEventListener("change", function () {
             const includeOTC = currentValue.includes('-OTC')
 
             if(includeAllOTC || includeAllAssets || isOTC || isAllAssets || (includeOTC && includeCrypto) || (isCrypto && !includeCrypto)){
-                selectedItems.splice(index, 1);
+                selectedPairs.splice(index, 1);
                 selectedItem.forEach((item) => {
                     item.remove()
                 });
@@ -42,8 +50,8 @@ select.addEventListener("change", function () {
             }
             item.appendChild(closeButton);
             closeButton.addEventListener("click", () => {
-                if(selectedItems.includes(value)){
-                    selectedItems.splice(index, 1);
+                if(selectedPairs.includes(value)){
+                    selectedPairs.splice(index, 1);
                     item.remove();
                 }
             });
@@ -52,10 +60,44 @@ select.addEventListener("change", function () {
         const selected = document.querySelectorAll('.selected-item')
         addOptions(selected)
         removeOption()
-        selectedItems.push(value);
+        selectedPairs.push(value);
     }
-    fetch('https://api.agbot.com.br/signal/v1/catalog').then(data => data).then(result => console.log(result))
 });
+
+catalogButton.addEventListener('click', () => {
+    const timezone = timezoneSelect.value
+    const direction = directionSelect.value
+    const timeframe = parseInt(timeframeSelect.value)
+    const today = (daylistSelect.value === 'false')? false : true
+    const accuracy = parseInt(accuracyInput.value)
+    const chosenCatalogDays = parseInt(catalogDays.value)
+    const martingaleSelected = parseInt(martingaleSelect.value)
+    const pairs = selectedPairs.map((pair) => (pair.includes('-BT'))? pair.replace('-BT', '') : pair)
+    const url = 'https://api.agbot.com.br/signal/v1/api/catalog'
+    const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json'}
+    const json = {
+        timezone,
+        pairs,
+        timeframe,
+        "number_of_days": chosenCatalogDays,
+        "percent": accuracy,
+        direction,
+        today,
+        "gale_level": martingaleSelected
+    }      
+
+    const fetchCatalog = async() => {
+        try {
+            const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(json)})
+            const data = await response.json()
+            console.log(data.signals)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    fetchCatalog()
+})
 
 $(document).ready(function () {
     $(".text-list").on("mousedown", function (e) {
