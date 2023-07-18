69f2986de9eb23b58a1cc61a435d351d7bab39ad
david-fernando <davidfernandodamata21@gmail.com>
2023-07-18 18:24:22 -0300

    Add removeSelectedItem

--- tools/js/catalog.js
+++ tools/js/catalog.js
@@ -184,10 +184,20 @@ catalogButton.addEventListener('click', async() => {
     showResult()
 })
 
+const returnCurrentDate = () => {
+  const date = new Date()
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = String(date.getFullYear()).slice(-2);
+  
+  return `${day}/${month}/${year}`;
+};
+
 function showResult(){
     const tableContainer = document.querySelector('.table-container')
     tableContainer.classList.add('border-light-color')
     const timeframe = parseInt(timeframeSelect.value)
+    const currentDate = returnCurrentDate()
 
     const content = `<header id="table-header">
     <div class="form-check">
@@ -216,14 +226,14 @@ function showResult(){
   tableContainer.addChildren(content)
   const tableField = document.querySelector('#table-field')
 
-  result.map((item) => {
-    const field = `<tr>
+  result.map((item, index) => {
+    const field = `<tr class="line-data" data-checked="false">
         <th scope="row">
         <div class="form-check">
-          <input class="form-check-input checkbox-option" onChange="handleCheckbox()" type="checkbox" value="" id="flexCheckIndeterminate">
+          <input class="form-check-input checkbox-option" onChange="handleCheckbox(${index})" type="checkbox" value="" id="flexCheckIndeterminate">
         </div>
       </th>
-      <td>13/07/23</td>
+      <td>${currentDate}</td>
       <td>${item[0]}</td>
       <td>${item[3]}</td>
       <td>${timeframe}M</td>
@@ -232,17 +242,24 @@ function showResult(){
   })
 }
 
-function handleCheckbox() {
+function handleCheckbox(lineIndex) {
     const selectCheckbox = document.querySelector('.select-checkbox')
     const isSelectAllCheckbox = selectCheckbox.classList.contains('select-all-checkbox')
     const checkboxOption = document.querySelectorAll('.checkbox-option')
+    const lineData = document.querySelectorAll('.line-data')
+    const isMarked = (lineData[lineIndex].dataset.checked === 'true')? true : false
     const isThereaMarkedCheckbox = [...checkboxOption].some(item => item.checked === true)
-    if(isSelectAllCheckbox && !isThereaMarkedCheckbox){
+    if(!isMarked){
+        lineData[lineIndex].dataset.checked = 'true'
+    }else {
+        lineData[lineIndex].dataset.checked = 'false'
+    }
+    if(!isThereaMarkedCheckbox){
         selectCheckbox.removeAttribute('checked')
         selectCheckbox.classList.remove('select-all-checkbox')
         return removeHeaderButton()
     }
-    if(isThereaMarkedCheckbox){
+    if(!isSelectAllCheckbox){
         selectCheckbox.setAttribute('checked', 'true')
         selectCheckbox.classList.add('select-all-checkbox')
         addHeaderButton()
@@ -252,17 +269,21 @@ function handleCheckbox() {
 function handleAllCheckbox() {
     const selectCheckbox = document.querySelector('.select-checkbox')
     const checkboxOption = document.querySelectorAll('.checkbox-option')
+    const lineData = document.querySelectorAll('.line-data')
     const isThereaMarkedCheckbox = [...checkboxOption].some(item => item.checked === true)
+
     const selectAllCheckbox = () => {
         selectCheckbox.setAttribute('checked', 'true')
         selectCheckbox.classList.add('select-all-checkbox')
         checkboxOption.forEach(item => item.checked = true)
+        lineData.forEach(item => item.dataset.checked = 'true')
         addHeaderButton()
     }
     const uncheckAllCheckbox = () => {
         selectCheckbox.removeAttribute('checked')
         selectCheckbox.classList.remove('select-all-checkbox')
         checkboxOption.forEach(item => item.checked = false)
+        lineData.forEach(item => item.dataset.checked = 'false')
         removeHeaderButton()
     }
     if(isThereaMarkedCheckbox){
@@ -271,9 +292,23 @@ function handleAllCheckbox() {
     return selectAllCheckbox()
 }
 
+function removeSelectedItem(){
+    const lineData = [...document.querySelectorAll('.line-data')]
+    const selectCheckbox = document.querySelector('.select-checkbox')
+    lineData.forEach((item) => {
+        const isMarked = (item.dataset.checked === 'true')? true : false
+        if(isMarked){
+            item.parentNode.removeChild(item)
+        }
+    })
+    selectCheckbox.removeAttribute('checked')
+    selectCheckbox.classList.remove('select-all-checkbox')
+    removeHeaderButton()
+}
+
 const addHeaderButton = () => {
     const tableHeader = document.querySelector('#table-header')
-    const buttons = `<button class="delete-button">
+    const buttons = `<button class="delete-button" onClick="removeSelectedItem()">
     <svg class="bi bi-trash delete-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
