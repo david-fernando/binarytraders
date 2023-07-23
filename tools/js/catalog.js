94179b7b5c92de665400b6f4554a16fd01341d08
david-fernando <davidfernandodamata21@gmail.com>
2023-07-23 16:52:40 -0300

    Align delete and copy icons

--- tools/js/catalog.js
+++ tools/js/catalog.js
@@ -387,13 +387,13 @@ Percentage: ${accuracy}%`
 const addHeaderButton = () => {
     const tableHeader = document.querySelector('#table-header')
     const buttons = `<button class="delete-button" title="Delete" onClick="removeSelectedItem()">
-    <svg class="bi bi-trash delete-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="17" fill="#0d6efd" viewBox="0 0 16 16">
+    <svg class="bi bi-trash delete-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0d6efd" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
     </svg>
   </button>
   <button class="copy-button" title="Copy" onClick="copyItems()">
-    <svg class="bi bi-clipboard I-icon copy-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="17" fill="#0d6efd" viewBox="0 0 16 16">
+    <svg class="bi bi-clipboard I-icon copy-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="#0d6efd" viewBox="0 0 16 16">
       <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
       <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
     </svg>
